d2a67be3cb5b06f603d8d991329307ee8208123a
larluo <laluo@clojurians.org>
2016-05-31 08:27:41 +0000

    add storm-wordcount example

---
+++ storm-wordcount/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to storm-wordcount
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ storm-wordcount/test/storm_wordcount/core_test.clj
@@ -0,0 +1,7 @@
+(ns storm-wordcount.core-test
+  (:require [clojure.test :refer :all]
+            [storm-wordcount.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
