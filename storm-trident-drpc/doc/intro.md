44553bd9175b93fd1250d722f7e240dc23018dbc
larluo <laluo@clojurians.org>
2016-06-01 09:56:43 +0000

    add storm trident drpc example

---
+++ storm-trident-drpc/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to storm-trident-drpc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ storm-trident-drpc/src/storm_trident_drpc/core.clj
@@ -0,0 +1,54 @@
+(ns storm-trident-drpc.core
+  (:require [clojure.string :as string]
+            [marceline.storm.trident :as t])
+  (:import [org.apache.storm LocalCluster LocalDRPC StormSubmitter]
+           [org.apache.storm.trident TridentTopology]
+           [org.apache.storm.trident.operation.builtin MapGet]
+           [org.apache.storm.trident.testing FixedBatchSpout MemoryMapState$Factory ])
+  (:gen-class))
+
+(def sentences ["lord ogdoad"
+                "master of level eight shadow world"
+                "the willing vessel offers forth its pure essence"])
+
+(t/deftridentfn split-args
+  [tuple coll]
+  (when-let [args (t/first tuple)]
+    (let [words (string/split args #" ")]
+      (doseq [word words] (t/emit-fn coll word)) )))
+
+(t/defcombineraggregator count-words
+  ([] 0)
+  ([tuple] 1)
+  ([t1 t2] (+ t1 t2)))
+
+(defn build-topology [drpc]
+  (let [trident-topology (TridentTopology.)
+        spout (doto (FixedBatchSpout. (t/fields "sentence") 3  (into-array (map t/values sentences)))
+                (.setCycle true))
+        word-counts (->  (t/new-stream trident-topology "word-counts" spout)
+                        (t/each ["sentence"] split-args ["word"])
+                        (t/group-by ["word"])
+                        (t/persistent-aggregate (MemoryMapState$Factory.) count-words ["count"]))]
+    (-> (t/drpc-stream trident-topology "words" drpc)
+        (t/each ["args"] split-args ["word"])
+        (t/project ["word"])
+        (t/group-by ["word"])
+        (t/state-query word-counts ["word"] (MapGet.) ["count"]))
+    trident-topology))
+
+(defn run-local! []
+  (let [cluster (LocalCluster.)
+        drpc (LocalDRPC.)]
+    (.submitTopology cluster "word-count" {} (.build  (build-topology drpc)))
+    (Thread/sleep 10000)
+    (let [results (.execute drpc "words" "evil vessel ogdoad")]
+      (.shutdown cluster)
+      results)))
+
+(defn submit-topology! [name]
+  (StormSubmitter/submitTopology name {} (.build (build-topology nil))))
+
+(defn -main
+  ([] (run-local!))
+  ([name] (submit-topology! name)))
