2f52a316e8b748788d9d90b3ac9975750c15da82
larry luo <larluo@larrys-MacBook-Pro.local>
2016-06-05 19:23:56 +0800

    add kafka-transfer

---
+++ kafka-transfer/test/kafka_transfer/core_test.clj
@@ -0,0 +1,7 @@
+(ns kafka-transfer.core-test
+  (:require [clojure.test :refer :all]
+            [kafka-transfer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
