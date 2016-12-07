3d3834a2590bd3ff07023760793daaf28afcf14b
spiderdt <larluo@clojurians.org>
2016-12-07 16:57:29 +0800

    add cascalog jdbc tap dep

--- cascalog-etl/src/cascalog_etl/core.clj
+++ cascalog-etl/src/cascalog_etl/core.clj
@@ -1,5 +1,5 @@
 (ns cascalog-etl.core
-  (:require [cascalog.api :refer [?- ??- <- ?<- ??<- stdout defmapfn defmapop mapfn]]
+  (:require [cascalog.api :refer [?- ??- <- ?<- ??<- stdout defmapfn mapfn defaggregatefn aggregatefn]]
             [cascalog.logic.def :refer [defmapcatfn]]
             [cascalog.logic.ops :as c ]
             [cascalog.playground :refer [sentence person age]]
@@ -21,20 +21,33 @@
        (r/map #(->> % (partition 9 9 [0]) first vec))
        (into [])))
 
-(defmapop square [&coll] (map #(hash-map (name coll) coll) coll))
-
-
 (def score-channel
   (<- [?selector ?dimension-metrics]
       ; [period dmbd bg bottler channel code item  fact value]
       ; ["Availability / 产品铺货", "SOVI / 排面占有率", "Cooler / 冰柜", "Activation / 渠道活动", "价格沟通.*"]
-      (score :> ?period ?dmbd ?bg ?bottler ?channel ?code ?channel "Score" ?value)
-      ((mapfn [header & coll] (map vector header coll))
-           [:period :bg :bottler] ?period ?bg ?bottler :> ?selector)
-      ((aggregatefn ([] {})
-                    ([acc val] )
-                    ([x] [x]))
-           [:channel :bg :value] ?channel ?bg ?value :> ?dimension-metrics)  ))
+      (score  :> ?period ?dmbd ?bg ?bottler ?channel ?code ?channel "Score" ?value)
+      (((fn [header] (mapfn [& coll] [(mapv vector header coll)] ))
+           [:period :bg :bottler]) ?period ?bg ?bottler :> ?selector)
+      (((fn [header] (aggregatefn ([] {})
+                                  ([acc & coll] (apply assoc-in acc ((juxt drop-last last) (mapv vector header coll))))
+                                  ([x] [x])))
+           [:channel :bg :score]) ?channel ?bg ?value :> ?dimension-metrics)  ))
 
+(defn mysql-tap []
+  (let [scheme (JDBCScheme. (Fields. (into-array String ["" "screen_name" "content"])) (into-array String ["id" "screen_name" "content"]))
+             table-desc (TableDesc. "tweets")
+             tap (JDBCTap. "jdbc:mysql://localhost:3306/burn?user=root&password=" "com.mysql.jdbc.Driver" table-desc scheme)]
+         tap))
+
+(def sink-mysql [])
 (?- (stdout) score-channel)
 
+(def score-test [["a" "E&D M/H / 中高档餐" "G13" 25.1535054339554]
+                 ["b"  "E&D Trad / 传统餐饮" "G18" 23.8869117480029] ] )
+(?<- (stdout)
+     [?selector ?dimension-metrics]
+     (score-test :> ?selector ?channel ?bg ?value)
+     (((fn [header] (aggregatefn ([] {})
+                                 ([acc & coll] (apply assoc-in acc ((juxt drop-last last) (map vector header coll))))
+                                 ([x] [x])))
+       [:channel :bg :score]) ?channel ?bg ?value  :> ?dimension-metrics))
