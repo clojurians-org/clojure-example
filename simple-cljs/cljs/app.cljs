b5aea818e7697e5e186230c8a15ba6caac2e0d69
clojurians-org <larluo@clojurians.org>
2017-05-29 19:21:08 +0800

    add form display

--- simple-cljs/cljs/app.cljs
+++ simple-cljs/cljs/app.cljs
@@ -20,12 +20,15 @@
 
 (defn load-dom []
   (dommy/set-html! (sel1 :body) 
-    (html ["div#app" ["div#tree"] ["div#table"]])))
+    (html ["div#app" ["div#tree-data" {:style "display:none;"}] ["div#tree.col-sm-12"] 
+                     ["div#table-data" {:style "display:none;"}] ["div#table.col-sm-12"]
+                     ["div#form-data" {:style "display:none;"}] ["div#form.col-sm-12"]])))
 
 (defn build-tree []
   (let [tree-edn [{:name "aa" :value "xx" :desc "aaa"}
                   {:name "bb" :value "yy" :desc "bbb"}
                   {:name "cc" :value "zz" :desc "ccc"}]
+        _ (dommy/set-text! (sel1 :#tree-data) (pr-str tree-edn))
         tree-view-edn (map-indexed (fn [id row] {:text (goog.string.format "[%s]=%s" id  (pr-str row)) 
                                                  :nodes (mapv (fn [[k v]] {:text (str k "=" v)}) row)}) tree-edn)]
     (.treeview (js/jQuery "#tree") (clj->js {:data tree-view-edn}))
@@ -35,19 +38,28 @@
   (let [table-edn [["aaa" "AAA" 50]
                    ["bbb" "BBB" 100]
                    ["ccc" "CCC" 70]]
-        table-dom [:table 
+        table-dom [:table.table.table-bordered.table-condensed
                    [:tbody (map-indexed (fn [id row] [:tr (map #(do [:td (str %)]) (cons id row))]) table-edn)]] ]
     (dommy/set-html! (sel1 :#table) (html table-dom) )  ))
 
+(defn build-form []
+  (let [form-edn ["aaa[AAA]" "bbb[BBB]" "ccc[CCC]"]
+        _ (dommy/set-text! (sel1 :#form-data) (pr-str form-edn))
+        form-dom [:div.row (map-indexed (fn [id row] [:div.form-group
+                                                      [:label.control-label.col-sm-2 {:for (goog.string.format "row-%s" id)} (str row ":")]
+                                                      [:input.col-sm-10 {:type "text" :id (goog.string.format "row-%s" id)}]])
+                              form-edn)]]
+    (dommy/set-html! (sel1 :#form) (html form-dom))  ))
 (defn load-data []
   (build-tree)
-  (build-table))
+  (build-table)
+  (build-form))
 
 (defn load-css []
   (dommy/set-html! (sel1 :head)
                    (html [:link {:rel "stylesheet" :href "ext/bootstrap-3.3.7-dist/css/bootstrap.min.css"}]
-                         [:style {:type "text/css"} 
-                          (css [:table :td {:border "1px solid black"}])])))
+                         [:style {:type "text/css"} "\n"
+                          (css [:#app {:margin "0 auto" :width "80%"}])])))
 
 (defn load-event [])
 
