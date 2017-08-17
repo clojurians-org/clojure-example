df1d663fdacb71a5b5e9a6298ed3615983745fad
clojurians-org <larluo@clojurians.org>
2017-08-17 13:01:45 +0800

    updat dep

--- yarn-runner/project.clj
+++ yarn-runner/project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-common "2.8.0"]
+                                       [org.apache.hadoop/hadoop-hdfs "2.8.0"]
+                                       [org.apache.hadoop/hadoop-yarn-api "2.8.0"]
+                                       [org.apache.hadoop/hadoop-yarn-client "2.8.0"]]}}
   :dependencies [[org.clojure/clojure "1.8.0"]])
