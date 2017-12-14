f8d07f5d03492e7b089ed3e780a28ddbaf432d70
clojurians-org <larluo@clojurians.org>
2017-12-14 16:29:20 +0800

    add spark-sql ha

---
+++ spark-sql/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to spark-sql
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ spark-sql/project.clj
@@ -0,0 +1,9 @@
+(defproject spark-sql "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.spark/spark-sql_2.11 "2.2.0"]
+                 [org.apache.spark/spark-hive_2.11 "2.2.0"]
+                 [org.apache.spark/spark-yarn_2.11 "2.2.0"]])
